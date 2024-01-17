1175bcc6f2134a415a3958ea210d79f349f7ad07
Mihkel Küla <mihkel.kula@tktk.ee>
2024-01-17 18:17:39 +0200

    add week7 code

---
+++ DiscountCalucalor/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("provide total: ");
+int userInput = Int32.Parse(Console.ReadLine());
+
+double discount = CalculateDiscount(userInput);
+
+Console.WriteLine($"Your discount is: {discount} %");
+
+double newTotal = userInput - (userInput * discount);
+
+Console.WriteLine($"Your new total is: {newTotal}");
+static double CalculateDiscount(int total)
+{
+    if(total > 10)
+    {
+        return 1;
+    }
+    else if(total >= 10 && total < 20)
+    {
+        return 5;
+    }
+    else
+    {
+        return 10;
+    }
+}
+
+static double CalculateDiscount(double total, double discount)
+{
+    double result = total - (total * discount) / 100;
+    return result;
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
